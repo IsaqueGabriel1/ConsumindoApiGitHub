6629d09bebc92ffc6f58fa1d6becd1d877a935ee
IsaqueGabriel1 <isaquegabriel2002@outlook.com>
2022-08-03 22:27:17 -0300

    Mudanças no CSS

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ function ApiGit(){
     }else{
         userName = teste.value
     }
-    
-    
+    //realizando uma requisição na api do github
     fetch(`https://api.github.com/users/${userName}`, {
        method: 'GET',
        headers: {
            Accept: 'application/vnd.github.v3+json',
        },
+       //caso haja exito na promessa, será retornado um json()
     }).then((resp) => {
        console.log(resp)
        return resp.json()
     }).then((data) =>{
-       
+       //aqui é consumido os dados da API
        let img = document.getElementById("tImg")
        let user = document.getElementById("txtUser")
        let bio = document.getElementById("txtBio")
@@ -29,7 +29,7 @@ function ApiGit(){
        qntRepo.innerText = data.public_repos
        img.src = data.avatar_url
     }).catch((data) =>{
-        
+        //Aqui será o tratamento de erro caso a requisição falhe
     })
 }
     
